5df980c952bf41350fd35c495e1d88c22a01c0d0
William Höök-Specker <william@rhyme.se>
2024-06-27 16:36:02 +0200

    fixes sidenav implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <SideNav />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SideNav />
+        {children}
+      </body>
     </html>
   )
 }
